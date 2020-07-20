0abed58638fbf9c7464dc96dc324073b6c0f8e4f
Dominik Resl <nikicresl@gmail.com>
2020-07-20 10:57:50 +0200

    Asgi update

--- django_react/asgi.py
+++ django_react/asgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
+import sys
+from os.path import join
 import django
 from channels.routing import get_default_application
 
+base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_react.settings')
+sys.path.append(join(base_dir))
 django.setup()
 application = get_default_application()
